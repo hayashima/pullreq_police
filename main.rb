a8d6af66f6a804c5d2bab42f87069aeca25d3fd4
rhiroe <ride.poke@gmail.com>
2022-09-22 14:27:40 +0900

    リリース番号で正確にソートする

--- main.rb
+++ main.rb
@@ -9,18 +9,19 @@ all_branches = []
 # 100件ずつしか取得できないので取得できなくなるまでループさせる
 loop.with_index(1) do |_, page|
   branches = client.branches(repo, per_page: 100, page: page, protected: protected).map(&:name)
-  break if branches.size == 0
-
   all_branches += branches
+  break if branches.size < 100
 end
 
-# リリースブランチの正規表現に合致するものだけに絞る
+# リリースブランチの正規表現に合致するものだけに絞り、リリース番号でソートする
+i = 0
 release_branches = all_branches.select { |branch_name| branch_name.match?(release_branch_regexp) }
+                               .sort_by { |v| [*v.scan(/\d+/).map(&:to_i), i += 1] }
 
-# 万が一でもリリースブランチ以外で実行されないように条件分岐
-if release_branches.include? current_branch
-  # リリース番号が現在より大きいものの中で最小のもの、なければ開発ブランチを取得
-  to_branch = release_branches.select(&current_branch.method(:<)).min || client.repo(repo).default_branch
-  # プルリクを作成
-  client.create_pull_request(repo, to_branch, current_branch, title, body)
-end
+index = release_branches.index(current_branch)
+exit(1) if index.nil? # indexがnilになることは基本的にありえないので異常系で終了させる
+
+# リリース番号的に次のブランチを取得、なければデフォルトブランチを取得
+to_branch = release_branches[index + 1] || client.repo(repo).default_branch
+# プルリクを作成
+client.create_pull_request(repo, to_branch, current_branch, title, body)
